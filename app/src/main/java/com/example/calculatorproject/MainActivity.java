22c1a4a55561b6466d6c4fed06655bd5c4b0efba
RiePM <85295501+RiePM@users.noreply.github.com>
2023-02-06 18:39:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/calculatorproject/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.calculatorproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
+
+
+//You can create a new class by adding it here: project - app - src - main - java - com.example (right click and add java class)
